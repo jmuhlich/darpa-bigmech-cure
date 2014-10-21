99f14b59e50a9b10b7076a9e753a65ddf8e2f17c
Jeremy Muhlich <jmuhlich@bitflood.org>
2014-10-21 11:58:03 -0400

    added single-receptor variant of egfr_example for easy visualization

---
+++ models/egfr_single.py
@@ -0,0 +1,22 @@
+"""Variant of egfr_example where adaptors only bind one EGFR monomer in the
+dimer, to make it feasible to visualize the species and reactions."""
+
+from pysb import *
+from pysb.core import ComponentSet
+
+import egfr_example
+
+Model(base=egfr_example.model)
+
+rules = ComponentSet()
+params = ComponentSet()
+for r in L_bind_R, R_R_dimerize, R_Y1068_transphos, R_Y1148_transphos:
+    rules.add(r)
+    params.add(r.rate_forward)
+    if r.rate_reverse:
+        params.add(r.rate_reverse)
+model.rules -= rules
+model.parameters -= params
+
+Initial(egfr(l=None, r=1, Y1068='pY', Y1148='pY') % egfr(l=None, r=1, Y1068='Y', Y1148='Y'),
+        egfr_tot)
